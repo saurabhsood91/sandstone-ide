2d82310da02c8298dbfab9c2527910da6c6bf8de
Zebula Sampedro <sampedro@colorado.edu>
2016-09-01 11:47:31 -0600

    Actually fixed cyclic dependency this time.

--- sandstone/apps/filebrowser/signals.py
+++ sandstone/apps/filebrowser/signals.py
@@ -1,8 +1,5 @@
-<<<<<<< d9ef475f0c0c98504a25860476c0720a7a6f3ac8
-=======
-from receivers import *
->>>>>>> Fixed circular dependency.
 from pydispatch import dispatcher
+
 
 # TODO This seems a little redundant
 # TODO find a better way
@@ -10,7 +7,7 @@ SIGNALS_HANDLED = (
     'filetree:init',
 )
 
-<<<<<<< d9ef475f0c0c98504a25860476c0720a7a6f3ac8
+
 # Receivers
 def filetree_init(sender):
     from posixfs import PosixFS
@@ -23,8 +20,6 @@ def filetree_expanded(sender):
     print 'Filetree Expanded'
 
 # Connect signals
-=======
->>>>>>> Fixed circular dependency.
 dispatcher.connect(filetree_init, signal='filetree:init')
 
 dispatcher.connect(filetree_expanded, signal='filetree:expanded')
